e7a5b7ec0a8192a45b8bf96c82c16a9f5adaafe1
Abdulrehman517 <abdulrehman517uetian@gmail.com>
2025-04-05 16:20:53 +0500

    tweaks for Email

--- actions/sendEmailSMTP.ts
+++ actions/sendEmailSMTP.ts
@@ -18,7 +18,9 @@ export default async function sendEmail(formData: FormData) {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASS,
